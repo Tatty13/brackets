c26e6e4958bf1c0b2f07cf919cc2a886a73b38ec
Tatty13 <tat.mi.iva@gmail.com>
2022-07-07 23:26:16 +0300

    feat: add task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let brackets = {};
+  for (let item of bracketsConfig) {
+    brackets[item[0]] = item[1];
+  }
+
+  let stek = [];
+  for (let i = 0; i < str.length; i++) {
+    let lastItem = stek[stek.length-1];
+    let current = str[i];
+    
+    if (current in brackets) {
+      (current == brackets[current]) && (current == lastItem) ? 
+          stek.pop() : stek.push(current);
+    } else {
+      if (stek.length === 0) {
+        return false;
+      }
+      brackets[lastItem] == current ? stek.pop() : false;
+    }
+  } 
+    return stek.length === 0;
 }
